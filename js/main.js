f951022dbeba7f6d9cd9ea5cfb204b11a1c5ce00
wilmer l <wmproyecciones@hotmail.com>
2022-07-13 13:39:17 -0500

    archivos originales

---
+++ js/main.js
@@ -0,0 +1,12 @@
+ let botonLogin=document.getElementById("head__login-botonid");
+
+
+
+
+
+    botonLogin.addEventListener(("click"),function(event){
+        window.location.href='formulario-login/login.html';
+    
+       });
+   
+ 
